a195dc490d5e3e5435f5a7adfd6bd0919d9a8119
cmj77 <miclark@paloaltonetworks.com>
2018-02-09 09:38:31 -0600

    remove unhealthy raw dump from develop

--- new/templates/shutiltests.py
+++
@@ -1,8 +0,0 @@
-
-from shutil import make_archive
-import os
-
-archive_name = os.path.expanduser(os.path.join('~', 'PycharmProjects/untitled/templates/bootstrap-archive'))
-root_dir = os.path.expanduser(os.path.join('~', 'PycharmProjects/untitled/templates/bootstrap'))
-make_archive(archive_name, 'zip', root_dir)
-
--- new/trash/iso.py
+++
@@ -1,9 +0,0 @@
-import os
-from subprocess import call
-
-
-
-call(["mkisofs", "-J", "-R", "-v", "-V", "'bootstrap'", "-A", "'bootstrap'", \ 
-"-ldots", "-l", "-allow-lowercase", "-allow-multidot", "-o", \
-"/home/ubuntu/automic/static/KVM/bootstrap-archive-kvm.iso", \
-"./static/KVM/bootstrap"])
